42331ec03e3b8c70d09aeeb619710341a72d73ed
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2022-12-15 13:59:01 -0500

    Pull data from the Recipe class

--- app/src/main/java/com/example/kakelandet_mycookbook/RecipeListViewModel.kt
+++ app/src/main/java/com/example/kakelandet_mycookbook/RecipeListViewModel.kt
@@ -3,5 +3,10 @@ package com.example.kakelandet_mycookbook
 import androidx.lifecycle.ViewModel
 
 class RecipeListViewModel : ViewModel() {
+    private val initialRecipesList = recipeList()
 
+
+    fun getRecipesList(): List<Recipe> {
+        return initialRecipesList
+    }
 }
