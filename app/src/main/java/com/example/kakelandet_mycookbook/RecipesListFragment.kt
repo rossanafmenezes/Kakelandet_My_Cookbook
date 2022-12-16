fddba3cb51f8a8abb947ceba23be982933132039
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2022-12-16 07:00:16 -0500

    Switch to clear binding on onDestroyView to avoid memory leak

--- app/src/main/java/com/example/kakelandet_mycookbook/RecipesListFragment.kt
+++ app/src/main/java/com/example/kakelandet_mycookbook/RecipesListFragment.kt
@@ -37,8 +37,8 @@ class RecipesListFragment : Fragment() {
         return binding.root
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
     }
 }
