04fdda4c2a1c5a6ec00eb021f49cd5f4f0dfe52b
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2022-12-19 07:18:09 -0500

    Clear binding on onDestroyView()

--- app/src/main/java/com/example/kakelandet_mycookbook/IngredientsListFragment.kt
+++ app/src/main/java/com/example/kakelandet_mycookbook/IngredientsListFragment.kt
@@ -35,4 +35,9 @@ class IngredientsListFragment : Fragment() {
 
         return binding.root
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
